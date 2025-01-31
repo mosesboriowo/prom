ef405f8eca46faa275fe0a68bbcf0393dd52114c
mosesboriowo <85451157+mosesboriowo@users.noreply.github.com>
2025-01-31 17:31:09 +0100

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -3,62 +3,65 @@
 # Update package lists
 sudo apt-get update
 
-# Install necessary packages
-sudo apt-get install -y wget tar
+sudo groupadd --system prometheus
 
-# Define Prometheus version
-PROMETHEUS_VERSION="2.33.5"
+sudo useradd -s /sbin/nologin --system -g prometheus prometheus
 
-# Create a user for Prometheus
-sudo useradd --no-create-home --shell /bin/false prometheus
-
-# Create necessary directories
-sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 
-# Download Prometheus
-cd /tmp
-wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
+for i in rules rules.d files_sd; do sudo mkdir -p /etc/prometheus/${i}; done
 
-# Extract Prometheus
-tar -xvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
+sudo apt update
+sudo apt -y install wget curl vim
 
-# Move binaries to /usr/local/bin
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus /usr/local/bin/
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/promtool /usr/local/bin/
+mkdir -p /tmp/prometheus && cd /tmp/prometheus
+curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
 
-# Move configuration files
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/consoles /etc/prometheus
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/console_libraries /etc/prometheus
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus.yml /etc/prometheus
+tar xvf prometheus*.tar.gz
 
-# Set ownership
-sudo chown -R prometheus:prometheus /etc/prometheus
-sudo chown -R prometheus:prometheus /var/lib/prometheus
+cd prometheus*/
 
-# Clean up
-rm -rf prometheus-${PROMETHEUS_VERSION}.linux-amd64*
+sudo mv prometheus promtool /usr/local/bin/
+
+
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo mv consoles/ console_libraries/ /etc/prometheus/
 cd ~
 
+sudo vim /etc/prometheus/prometheus.yml
+
+
 # Create Prometheus systemd service file
-sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/prometheus.service<<EOF
 [Unit]
 Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
 Wants=network-online.target
 After=network-online.target
 
 [Service]
+Type=simple
 User=prometheus
 Group=prometheus
-Type=simple
-ExecStart=/usr/local/bin/prometheus \\
-  --config.file /etc/prometheus/prometheus.yml \\
-  --storage.tsdb.path /var/lib/prometheus/ \\
-  --web.console.templates=/etc/prometheus/consoles \\
-  --web.console.libraries=/etc/prometheus/console_libraries
+ExecReload=/bin/kill -HUP \$MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# Reload
+for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
+for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
+sudo chown -R prometheus:prometheus /var/lib/prometheus/
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
